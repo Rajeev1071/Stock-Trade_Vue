d6524821032e5e8773995e05948d66e6f96d1090
Rajeev1071 <rajeeevranjan.1071@gmail.com>
2021-10-20 20:20:54 +0530

    stock trader in vue

---
+++ README.md
@@ -0,0 +1,31 @@
+### Stock Trader With Vue
+
+Stock Trader Web App Using Vue JS.
+
+### Features
+
+* You May Save & Load Your Data
+* Click on 'End Day' To Begin New Day!
+* Live Profit or Loss Status
+* Stock Price Changes Everyday! Which Makes More Fun!
+
+### Technologies Used
+
+* ```Vue JS 2```
+* ```Twitter Bootstrap``` - To Look Things Good!
+* ```Vuex``` - Better State Management
+* ```Vue Router``` - To Find The Way!
+* ```Vue Resource``` - The Way To Talk With Server
+* ```Firebase``` - Store Records
+
+### Installation & Use
+
+* Download or Clone This Repository.
+* Enter Into Project Repository Folder
+* Install Required Packages Using NPM By This Command - 
+```npm install```
+* Run Project On Local Envirment By Running This Command -
+```npm run dev```
+* To Deploy For Production Run This Command - 
+```npm run build```
+
---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <h1><i class="fa fa-line-chart" aria-hidden="true"></i> Stock Trader Made With Vue JS 2<br>
+    <small><i class="fa fa-user" aria-hidden="true"></i> By Rajeev</small></h1>
+    <br>
+    <div class="row">
+        <div class="col-md-6">
+            <h3>Features</h3>
+            <li>You May Save & Load Your Data</li>
+            <li>Click on 'End Day' To Begain New Day!</li>
+            <li>Live Profit or Loss Status</li>
+            <li>Stock Price Changes Everyday! Which Makes More Fun!</li><br>
+            <router-link to="/stocks" class="btn btn-primary btn-lg" tag="button"><i class="fa fa-bolt" aria-hidden="true"></i> Start Trading!</router-link>
+        </div>
+        <div class="col-md-6">
+            <img src="https://camo.githubusercontent.com/224f79940611c6c12fb649128eca1cae31086d23/68747470733a2f2f7261776769742e636f6d2f7675656a732f617765736f6d652d7675652f6d61737465722f6c6f676f2e706e67"
+            class="img-responsive" width="300px" height="300px">
+        </div>
+    </div>
+    <hr>
+    <br>
+    <p><i class="fa fa-money" aria-hidden="true"></i> Your Funds: <strong>{{ funds | currency }}</strong></p>
+</div>
+</template>
+
+<script>
+export default {
+    computed: {
+        funds() {
+            return this.$store.getters.funds;
+        }
+    }
+}
+</script>
+
